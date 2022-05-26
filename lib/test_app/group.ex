e29b3fc9e16303416810502d8653b3963007a113
TvoiSenpai12 <sobolev.shurik@mail.ru>
2022-05-26 17:44:28 +0300

    Added poop version example 1

--- lib/test_app/group.ex
+++ lib/test_app/group.ex
@@ -1,21 +1,16 @@
 defmodule TestApp.Group do
   def group(mass) do
-    int = []
-    str = []
-    flo = []
-    arr = %{}
-
-    new_arr =
-    for n <- mass, do:
-    if is_integer(n), do: 
-      Map.put(arr, :integer, [n | int]), 
-    else: 
-      if is_bitstring(n), do: 
-        Map.put(arr, :string, [n | str]), 
-      else: Map.put(arr, :float, [n | flo])
+    arr = %{:string => [], :integer => [], :float => []}
     
-      new_arr
+    Enum.reduce(mass, fn n, acc ->
+      IO.inspect(arr) 
+      case n do
+        n when is_binary(n) -> Map.put(arr, :string, [n | acc])
+        n when is_integer(n) -> Map.put(arr, :integer, [n | acc])
+        n when is_float(n) -> Map.put(arr, :float, [n | acc]) 
+      end
+    end)
+    |> IO.inspect(arr)
   end
 end
-
 # TestApp.Group.group([1,2,"foo", 2.5, "bar", 6.3])
