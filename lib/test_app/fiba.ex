78ff755378995e3d17ddb78edaa3a3ebccc648a2
TvoiSenpai12 <sobolev.shurik@mail.ru>
2022-05-26 13:59:49 +0300

    Added for and ifelse in group

--- lib/test_app/fiba.ex
+++ lib/test_app/fiba.ex
@@ -2,6 +2,7 @@ defmodule TestApp.Fiba do
   def find(param) do
     list = [1, 1]
     fib(list, param)
+    # a = fib(list, param)
   end
 
   def fib(list, 2) do
@@ -10,9 +11,13 @@ defmodule TestApp.Fiba do
   end
 
   def fib(list, n) do
-    IO.puts(n)
-    if [hd(list) + hd(tl(list))] < n, do: fib([hd(list) + hd(tl(list))] ++ list, n - 1), else: list ++ [list, n - 1]
+    # IO.puts(n)
+    a = fib([hd(list) + hd(tl(list))] ++ list, n - 1)
+    # IO.inspect a, label: "List"
+    # if [hd(list) + hd(tl(list))] > n, do: fib([hd(list) + hd(tl(list))] ++ list, n - 1), else: list ++ [n]
   end
 end
+
+# a = TestApp.Fiba.find(n)
 
 # TestApp.Fiba.find(9)
--- lib/test_app/group.ex
+++ lib/test_app/group.ex
@@ -1,5 +1,16 @@
 defmodule TestApp.Group do
   def group(mass) do
-    mass
+    arr = %{}
+
+    for n <- mass, do:
+    if is_integer(n), do: 
+      Map.put_new(arr, :integer, n), 
+    else: 
+      if is_bitstring(n), do: 
+        Map.put_new(arr, :string, n), 
+      else: Map.put_new(arr, :float, n)
+
   end
 end
+
+# TestApp.Group.group([1,2,"foo", 2.5, "bar", 6.3])
