0cbeb943ac8b57e3149cd59dbde77bb7ab6bad17
TvoiSenpai12 <sobolev.shurik@mail.ru>
2022-05-26 18:57:01 +0300

    Added beta poop

--- lib/test_app/fiba.ex
+++ lib/test_app/fiba.ex
@@ -1,24 +1,12 @@
 defmodule TestApp.Fiba do
-  def find(param) do
-    list = [1, 1]
-    fib(list, param)
-    # a = fib(list, param)
-  end
-
-  def fib(list, 2) do
-    Enum.reverse(list)
-    # list
-  end
-
-  def fib(list, n) do
-    # IO.puts(n)
-    fib([hd(list) + hd(tl(list))] ++ list, n - 1)
-    # for i <- a, do: if i > n, do: Process.exit(self, :normal)
-    # IO.inspect a, label: "List"
-    # if [hd(list) + hd(tl(list))] > n, do: fib([hd(list) + hd(tl(list))] ++ list, n - 1), else: list ++ [n]
-  end
+  # defp do_fib do:  end
+  # defp do_fib do: end
+  # def fib([1,1] = list, param) do do_fib(list, param) end
 end
 
-# a = TestApp.Fiba.find(n)
-
 # TestApp.Fiba.find(9)
+
+
+# [a,b | _ _ ]
+# Enim.reverse
+# when
--- lib/test_app/group.ex
+++ lib/test_app/group.ex
@@ -2,12 +2,12 @@ defmodule TestApp.Group do
   def group(mass) do
     arr = %{:string => [], :integer => [], :float => []}
     
-    Enum.reduce(mass, fn n, acc ->
+    Enum.reduce(mass, acc, fn(n, acc) ->
       IO.inspect(arr) 
       case n do
-        n when is_binary(n) -> Map.put(arr, :string, [n | acc])
-        n when is_integer(n) -> Map.put(arr, :integer, [n | acc])
-        n when is_float(n) -> Map.put(arr, :float, [n | acc]) 
+        n when is_binary(n) -> Map.put(acc, :string, acc.string)
+        n when is_integer(n) -> Map.put(acc, :integer, acc.integer)
+        n when is_float(n) -> Map.put(acc, :float, acc.float) 
       end
     end)
     |> IO.inspect(arr)
