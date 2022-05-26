a7f0f10dc0886b56f24e02cf026d1c1753edc331
TvoiSenpai12 <sobolev.shurik@mail.ru>
2022-05-26 04:03:50 +0300

    Changed v2

--- lib/test_app/fiba.ex
+++ lib/test_app/fiba.ex
@@ -6,9 +6,13 @@ defmodule TestApp.Fiba do
 
   def fib(list, 2) do
     Enum.reverse(list)
+    # list
   end
 
   def fib(list, n) do
-    fib([hd(list) + hd(tl(list))] ++ list, n - 1)
+    IO.puts(n)
+    if [hd(list) + hd(tl(list))] < n, do: fib([hd(list) + hd(tl(list))] ++ list, n - 1), else: list ++ [list, n - 1]
   end
 end
+
+# TestApp.Fiba.find(9)
