e5936dbda78368ac3a523f07800d39adbbcc77b4
TvoiSenpai12 <sobolev.shurik@mail.ru>
2022-05-27 19:20:28 +0300

    Added new ver

--- lib/test_app/group.ex
+++ lib/test_app/group.ex
@@ -5,12 +5,12 @@ defmodule TestApp.Group do
     Enum.reduce(mass, fn(n, acc) ->
       IO.inspect(acc) 
       case n do
-        n when is_binary(n) -> Map.put(arr, :string, [n | acc])
-        n when is_integer(n) -> Map.put(arr, :integer, [n | acc])
-        n when is_float(n) -> Map.put(arr, :float, [n | acc])
+        n when is_binary(n) -> Map.put(arr, :string, [acc | n])
+        n when is_integer(n) -> Map.put(arr, :integer, [acc | n])
+        n when is_float(n) -> Map.put(arr, :float, [acc | n])
       end
     end)
-    # |> IO.inspect(arr)
+    |> IO.inspect
   end
 end
 # TestApp.Group.group([1,2,"foo", 2.5, "bar", 6.3])
