712d3e34098275ddf5050be1fbab935b2c707773
TvoiSenpai12 <sobolev.shurik@mail.ru>
2022-05-30 18:43:21 +0300

    Added final version projects

--- lib/test_app/fiba.ex
+++ lib/test_app/fiba.ex
@@ -7,15 +7,15 @@ defmodule TestApp.Fiba do
     defp do_fib(list, 2) do
       Enum.reverse(list)
     end
-
-  defp do_minus(list, n) do
-    a = do_fib([hd(list) + hd(tl(list))] ++ list, n - 1)
-    temp = Enum.map(a, fn(x) ->  if x > n do x end end)
-    Enum.reject(temp, &is_nil/1)
-  end
   
     defp do_fib(list, n) do
     do_fib([hd(list) + hd(tl(list))] ++ list, n - 1)
+    end
+
+    defp do_minus(list, n) do
+      a = do_fib([hd(list) + hd(tl(list))] ++ list, n - 1)
+      temp = Enum.map(a, fn(x) ->  if x > n do x end end)
+      Enum.reject(temp, &is_nil/1)
     end
 end
 
