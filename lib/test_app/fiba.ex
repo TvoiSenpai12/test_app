5ee77507a0341c2dddc9a8e51bc3e1b597c7a24d
TvoiSenpai12 <sobolev.shurik@mail.ru>
2022-05-30 17:34:52 +0300

    Added final fiba

--- lib/test_app/fiba.ex
+++ lib/test_app/fiba.ex
@@ -1,9 +1,22 @@
 defmodule TestApp.Fiba do
-  # def fib([1,1] = list, param) do do_fib(list, param) end
+    def find(param) do
+      list = [1, 1]
+      do_fib(list, param) -- do_minus(list, param)
+    end
+  
+    defp do_fib(list, 2) do
+      Enum.reverse(list)
+    end
 
-  # defp do_fib do: end
-
-  # defp do_fib do: end
+  defp do_minus(list, n) do
+    a = do_fib([hd(list) + hd(tl(list))] ++ list, n - 1)
+    temp = Enum.map(a, fn(x) ->  if x > n do x end end)
+    minus = Enum.reject(temp, &is_nil/1)
+  end
+  
+    defp do_fib(list, n) do
+    do_fib([hd(list) + hd(tl(list))] ++ list, n - 1)
+    end
 end
 
 # TestApp.Fiba.find(9)
@@ -12,3 +25,9 @@ end
 # [a,b | _ _ ]
 # Enim.reverse
 # when
+
+  # def fib([1,1] = list, param) do do_fib(list, param) end
+
+  # defp do_fib do: end
+
+  # defp do_fib do: end
